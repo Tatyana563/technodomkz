145e0f88b99676b60d19ecefeb727116d6548601
Tanya <tatyana.petrenko@dstarlab.com>
2020-10-07 16:09:31 +0300

    empty

---
+++ src/main/java/com/example/technodomkz/TechnodomkzApplication.java
@@ -0,0 +1,13 @@
+package com.example.technodomkz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnodomkzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnodomkzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/technodomkz/TechnodomkzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technodomkz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnodomkzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
