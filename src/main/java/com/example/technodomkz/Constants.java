d5c274ce6a1c2f51c73d8f538d46182a329fc6e6
Tanya <tatyana.petrenko@dstarlab.com>
2020-10-27 16:54:41 +0200

    constants changed

--- src/main/java/com/example/technodomkz/Constants.java
+++ src/main/java/com/example/technodomkz/Constants.java
@@ -4,4 +4,10 @@ public interface Constants {
     String URL = "https://www.technodom.kz";
     String ALL_SUFFIX = "/all";
     String CATEGORIES_URL = URL + ALL_SUFFIX;
+     long ONE_SECOND_MS = 1000L;
+     long ONE_MINUTE_MS = 60 * ONE_SECOND_MS;
+     long ONE_HOUR_MS = 60 * ONE_MINUTE_MS;
+     long ONE_DAY_MS = 24 * ONE_HOUR_MS;
+     long ONE_WEEK_MS = 7 * ONE_DAY_MS;
+     long INITIAL_DELAY = 1200;
 }
